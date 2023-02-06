46a521b601387e1fbd9e176e0644ea5aac11f299
being887 <59926850+being887@users.noreply.github.com>
2023-02-06 16:16:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,7 @@ def download():
     video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     video_filename = video.default_filename
     video.download()
-    app.logger.debug(f'Downloaded file: {video_filename}')
-    try:
-        return send_file(video_filename, as_attachment=True)
-    except Exception as e:
-        return str(e)
+   
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=9000)
+   app.run()

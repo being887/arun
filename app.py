d55e43c05a40e9701be526f524edc83fbe0e8579
being887 <59926850+being887@users.noreply.github.com>
2023-02-07 10:06:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ def download():
         yt = YouTube(url)
         video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
         video_filename = video.default_filename
-        video.download(output_path='./downloads/')
-        return "video is downloaded successfully"
+        response = make_response(send_file(video_filename, as_attachment=True))
+        response.headers["Content-Disposition"] = "attachment; filename={}".format(video_filename)
+        return response
     except Exception as e:
         return "An error occurred while downloading the video: {}".format(str(e))
+
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0",port=5000)

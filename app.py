41d853c383064e8f9561a39bf11a235030b56544
being887 <59926850+being887@users.noreply.github.com>
2023-02-07 12:45:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,7 @@ app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
-    if request.method == "POST":
-        session['link'] = request.form.get('url')
-        try:
-            url = YouTube(session['link'])
-            url.check_availability()
-        except:
-            return render_template("error.html")
-        return render_template("download.html", url = url)
+    
     return render_template("home.html")
 
 @app.route("/download", methods = ["GET", "POST"])
